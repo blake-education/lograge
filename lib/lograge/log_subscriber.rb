30d842c61304b0b7076b919a7bfaa2b3645f200b
Simon Coffey <simon@tribesports.com>
2012-09-19 13:34:32 +0100

    Handle Rails 3.0.x request format payload

--- lib/lograge/log_subscriber.rb
+++ lib/lograge/log_subscriber.rb
@@ -5,7 +5,9 @@ module Lograge
   class RequestLogSubscriber < ActiveSupport::LogSubscriber
     def process_action(event)
       payload = event.payload
+
       message = "[END] #{payload[:method]} #{payload[:path]} format=#{payload[:format]} action=#{payload[:params]['controller']}##{payload[:params]['action']}"
+
       message << extract_status(payload)
       message << runtimes(event)
       message << location(event)
@@ -19,6 +21,14 @@ module Lograge
     end
 
     private
+
+    def extract_format(payload)
+      if ::ActionPack::VERSION::MINOR == 0
+        payload[:formats].first
+      else
+        payload[:format]
+      end
+    end
 
     def extract_status(payload)
       if payload[:status]
